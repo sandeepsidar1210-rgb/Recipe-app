d07915363b9d40213fac6d7cf3914ed3312279db
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-02-03 23:54:49 +0530

    media load through predefined code

--- Django/settings.py
+++ Django/settings.py
@@ -118,7 +118,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+import os
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR, "public/static")
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- Django/urls.py
+++ Django/urls.py
@@ -16,6 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+# Serve static files
+urlpatterns += staticfiles_urlpatterns()
+
 
 from VEGE.views import *
 
@@ -24,3 +37,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('receipes/', receipes , name="receipes")
 ]
+
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
