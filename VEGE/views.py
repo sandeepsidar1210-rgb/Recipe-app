64b9e4903e2ec36323bf7bdf3745f59a4da02f07
Sandeep Sidar <sandeepsidar1210-rgb@users.noreply.github.com>
2026-02-09 05:22:55 +0530

    login page linking to home page

--- VEGE/views.py
+++ VEGE/views.py
@@ -3,7 +3,7 @@ from .models import *
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-
+from django.contrib.auth import authenticate , login
 # Create your views here.
 
 def receipes(request):
@@ -60,6 +60,25 @@ def delete_receipe(request ,id):
     return redirect('/receipes/')
 
 def login_page(request):
+    if request.method == "POST":
+        username =request.POST.get('username')
+        password =request.POST.get('password')
+
+        if not User.objects.filter(username = username).exists():
+            messages.info(request, "Invalid Username")
+
+        user = authenticate(username = username , password = password)
+
+        if user is None:
+            messages.error(request,'Invalid Password')
+            return redirect('/login/')
+        
+        else:
+            login(request , user)
+            return redirect('/receipes/')
+    
+
+
     return render(request, 'login.html')
 
 
